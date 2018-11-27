6ec5a49bf8a5f10da009702f450b7a2b1c945ea6
achgjack <zxzhustieee@163.com>
2018-11-27 10:48:31 +0800

    add ex1 folder

---
+++ ex1/ex1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/* this is a comment*/
+int main(int argc, char *argv[])
+{
+	int distance = 100;
+	int age = 21;
+	int height = 72;
+	// this is also a comment
+	printf("You are %d miles away.\n", distance);
+	printf("I am %d inches tall.\n", height);
+	printf("I am %d years old.\n", age);
+	printf("hello, world!\n");
+
+	return 0;
+}
---
+++ ex1/ex3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/* this is a comment*/
+int main(int argc, char *argv[])
+{
+	int age = 21;
+	int height = 72;
+	// this is also a comment
+	printf("I am %d inches tall.\n", height);
+	printf("I am %d years old.\n", age);
+	printf("hello, world!\n");
+
+	return 0;
+}

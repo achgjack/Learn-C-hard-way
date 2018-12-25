5b3be9bc454b33d83284210b3cdf74584eba6120
achgjack <zxzhustieee@163.com>
2018-12-25 17:48:39 +0800

    dos to unix

--- ex18.c
+++ ex18.c
@@ -5,16 +5,16 @@
 
 void die(const char *message)
 {
-	if (errno)
-	{
-		perror(message);
-	}
-	else
-	{
-		printf("ERROR: %s\n", message);
-	}
+    if (errno)
+    {
+        perror(message);
+    }
+    else
+    {
+        printf("ERROR: %s\n", message);
+    }
 
-	exit(1);
+    exit(1);
 }
 
 //  a typedef creates a fake type, in this 
@@ -22,105 +22,105 @@ void die(const char *message)
 typedef int (*compare_cb) (int a, int b);
 
 /*
-A classic bubble sort function that uses the
-compare_cb to do the sorting
+  A classic bubble sort function that uses the
+  compare_cb to do the sorting
 */
 int *bubble_sort(int *numbers, int count, compare_cb cmp)
 {
-	int temp = 0;
-	int i = 0;
-	int j = 0;
-	int *target = malloc(count * sizeof(int));
+    int temp = 0;
+    int i = 0;
+    int j = 0;
+    int *target = malloc(count * sizeof(int));
 
-	if (!target)
-	{
-		die("Memory error.");
-	}
+    if (!target)
+    {
+        die("Memory error.");
+    }
 
-	memcpy(target, numbers, count * sizeof(int));
+    memcpy(target, numbers, count * sizeof(int));
 
-	for (i = 0; i < count; i++) {
-		for (j = 0; j < count - 1; j++) {
-			if (cmp(target[j], target[j+1]) > 0) {
-				temp = target[j+1];
-				target[j+1] = target[j];
-				target[j] = temp;
-			}
-		}
-	}
+    for (i = 0; i < count; i++) {
+        for (j = 0; j < count - 1; j++) {
+            if (cmp(target[j], target[j+1]) > 0) {
+                temp = target[j+1];
+                target[j+1] = target[j];
+                target[j] = temp;
+            }
+        }
+    }
 
-	return target;
+    return target;
 }
 
 int sorted_order(int a, int b)
 {
-	return a - b;
+    return a - b;
 }
 
 int reverse_order(int a, int b)
 {
-	return b - a;
+    return b - a;
 }
 
 int strange_order(int a, int b)
 {
-	if (a == 0 || b == 0)
-	{
-		return 0;
-	}
-	else 
-	{
-		return a % b;
-	}
+    if (a == 0 || b == 0)
+    {
+        return 0;
+    }
+    else 
+    {
+        return a % b;
+    }
 }
 
 
 void test_sorting(int *numbers, int count, compare_cb cmp)
 {
-	int i = 0;
-	int *sorted = bubble_sort(numbers, count, cmp);
+    int i = 0;
+    int *sorted = bubble_sort(numbers, count, cmp);
 
-	if (!sorted)
-	{
-		die("Failed to sort as requested.");
-	}
+    if (!sorted)
+    {
+        die("Failed to sort as requested.");
+    }
 
-	for (i = 0; i < count; i++)
-	{
-		printf("%d\n", sorted[i]);
-	}
-	printf("\n");
+    for (i = 0; i < count; i++)
+    {
+        printf("%d\t", sorted[i]);
+    }
+    printf("\n");
 
-	free(sorted);
+    free(sorted);
 }
 
 int main(int argc, char *argv[])
 {
-	if (argc < 2)
-	{
-		die("USAGE: ex18 4 3 1 5 6");
-	}
+    if (argc < 2)
+    {
+        die("USAGE: ex18 4 3 1 5 6");
+    }
 
-	int count = argc - 1;
-	int i = 0;
-	char **inputs = argv + 1;
+    int count = argc - 1;
+    int i = 0;
+    char **inputs = argv + 1;
 
-	int *numbers = malloc(count * sizeof(int));
-	if (!numbers)
-	{
-		die("Memory error.");
-	}
+    int *numbers = malloc(count * sizeof(int));
+    if (!numbers)
+    {
+        die("Memory error.");
+    }
 
-	for (i = 0; i < count; i++)
-	{
-		numbers[i] = atoi(inputs[i]);
-	}
+    for (i = 0; i < count; i++)
+    {
+        numbers[i] = atoi(inputs[i]);
+    }
 
-	test_sorting(numbers, count, sorted_order);
-	test_sorting(numbers, count, reverse_order);
-	test_sorting(numbers, count, strange_order);
+    test_sorting(numbers, count, sorted_order);
+    test_sorting(numbers, count, reverse_order);
+    test_sorting(numbers, count, strange_order);
 
-	free(numbers);
+    free(numbers);
 
-	return 0;
+    return 0;
 }
